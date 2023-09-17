703c39897785054750ff63b4a33df74d39e71420
mahagarmr <mahagarmr@gmail.com>
2023-09-17 13:21:02 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CenterLine = new System.Windows.Forms.PictureBox();
+            this.Circle = new System.Windows.Forms.PictureBox();
+            this.Rectangle_2 = new System.Windows.Forms.PictureBox();
+            this.Rectangle_1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CenterLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Circle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rectangle_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rectangle_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CenterLine
+            // 
+            this.CenterLine.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CenterLine.Image = global::WindowsFormsApp2.Properties.Resources._106_1061890_vertical_line_08_08_statistical_graphics_removebg_preview;
+            this.CenterLine.Location = new System.Drawing.Point(371, 0);
+            this.CenterLine.Name = "CenterLine";
+            this.CenterLine.Size = new System.Drawing.Size(56, 451);
+            this.CenterLine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CenterLine.TabIndex = 5;
+            this.CenterLine.TabStop = false;
+            // 
+            // Circle
+            // 
+            this.Circle.Image = global::WindowsFormsApp2.Properties.Resources.Белый_квадрат;
+            this.Circle.Location = new System.Drawing.Point(391, 210);
+            this.Circle.Name = "Circle";
+            this.Circle.Size = new System.Drawing.Size(17, 20);
+            this.Circle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Circle.TabIndex = 4;
+            this.Circle.TabStop = false;
+            // 
+            // Rectangle_2
+            // 
+            this.Rectangle_2.Image = global::WindowsFormsApp2.Properties.Resources.Белый_квадрат;
+            this.Rectangle_2.Location = new System.Drawing.Point(776, 183);
+            this.Rectangle_2.Name = "Rectangle_2";
+            this.Rectangle_2.Size = new System.Drawing.Size(12, 84);
+            this.Rectangle_2.TabIndex = 3;
+            this.Rectangle_2.TabStop = false;
+            // 
+            // Rectangle_1
+            // 
+            this.Rectangle_1.Image = global::WindowsFormsApp2.Properties.Resources.Белый_квадрат;
+            this.Rectangle_1.Location = new System.Drawing.Point(12, 173);
+            this.Rectangle_1.Name = "Rectangle_1";
+            this.Rectangle_1.Size = new System.Drawing.Size(12, 84);
+            this.Rectangle_1.TabIndex = 2;
+            this.Rectangle_1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(159, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(616, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Circle);
+            this.Controls.Add(this.CenterLine);
+            this.Controls.Add(this.Rectangle_2);
+            this.Controls.Add(this.Rectangle_1);
+            this.Name = "Form1";
+            this.Text = "pingpong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.CenterLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Circle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rectangle_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rectangle_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Rectangle_1;
+        private System.Windows.Forms.PictureBox Rectangle_2;
+        private System.Windows.Forms.PictureBox Circle;
+        private System.Windows.Forms.PictureBox CenterLine;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private const int STEP = 10;
+        private const int BOT_STEP = 3;
+
+        private double deltaTime = 0.16;
+        private const double DELTA_TIME = 0.16;
+
+        private int Vx = 10;
+        private int Vy = 10;
+
+        private Point CircleStartLocation = new Point(391, 210);
+        private Point Rectangle1StartLocation = new Point(12, 173);
+        private Point Rectangle2StartLocation = new Point(776, 183);
+
+        private bool isBotCatch;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            KeyDown += Form1_KeyDown;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (timer1.Enabled == false) timer1.Start();
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    if (Rectangle_1.Location.Y <= 0) break;
+                    moveRectangle(Rectangle_1, 0, -STEP);
+                    break;
+
+                case Keys.S:
+                    if (Rectangle_1.Location.Y >= 367) break;
+                    moveRectangle(Rectangle_1, 0, STEP);
+                    break;
+            }
+        }
+
+        private void moveRectangle(PictureBox rectangle, int x, int y)
+        {
+            rectangle.Location = new Point(rectangle.Location.X + x, rectangle.Location.Y + y);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Circle.Location = new Point(Circle.Location.X + Convert.ToInt32(Vx * deltaTime), Circle.Location.Y);
+            Circle.Location = new Point(Circle.Location.X, Circle.Location.Y + Convert.ToInt32(Vy * deltaTime));
+
+
+            // Відбивання від верхніх стінок
+            if (Circle.Location.Y <= 0 || Circle.Location.Y >= 431)
+            {
+                deltaTime += 0.03;
+                Vy = -Vy;
+            }
+
+            // Відбивання від ракеток
+            if (Circle.Bounds.IntersectsWith(Rectangle_1.Bounds) || Circle.Bounds.IntersectsWith(Rectangle_2.Bounds))
+            {
+                deltaTime += 0.03;
+                Vx = -Vx;
+            }
+
+            if (Circle.Bounds.IntersectsWith(Rectangle_2.Bounds))
+            {
+                isBotCatch = true;
+            }
+
+            // Завершення раунду
+            if (Circle.Location.X <= 0 || Circle.Location.X >= 783)
+            {
+                if (Circle.Location.X <= 0) label2.Text = label2.Text = Convert.ToString(Convert.ToInt32(label2.Text) + 1);
+                else if (Circle.Location.X >= 783) label1.Text = label1.Text = Convert.ToString(Convert.ToInt32(label1.Text) + 1);
+
+                Circle.Location = CircleStartLocation;
+                Rectangle_1.Location = Rectangle1StartLocation;
+                Rectangle_2.Location = Rectangle2StartLocation;
+
+                deltaTime = DELTA_TIME;
+
+                timer1.Stop();
+            }
+
+            // Поведінка бота
+            if (Circle.Location.X < 391) isBotCatch = false; 
+
+            if (isBotCatch == true)
+            {
+                if (Rectangle_2.Location.Y < Rectangle2StartLocation.Y) moveRectangle(Rectangle_2, 0, BOT_STEP);
+                else if (Rectangle_2.Location.Y > Rectangle2StartLocation.Y) moveRectangle(Rectangle_2, 0, -BOT_STEP);
+            }
+
+            if (Circle.Location.X > 391 && isBotCatch == false)
+            {
+                if (Circle.Location.Y > Rectangle_2.Location.Y)
+                {
+                    if (Rectangle_2.Location.Y <= 367)
+                    {
+                        moveRectangle(Rectangle_2, 0, BOT_STEP);
+                    }
+                }
+                else if (Circle.Location.Y < Rectangle_2.Location.Y + Rectangle_2.Height)
+                {
+                    if (Rectangle_2.Location.Y >= 0)
+                    {
+                        moveRectangle(Rectangle_2, 0, -BOT_STEP);
+                    }
+                }
+            }
+
+        }
+    }
+}
